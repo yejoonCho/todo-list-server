63adc220b8ed3b05519fa8ae00937401ec4c514a
yejoon Cho <uactye@LAPTOP-QEMTIR7D.localdomain>
2021-12-14 21:33:52 +0900

    connect to Mysql

--- database.py
+++ database.py
@@ -1,7 +1,19 @@
+import os
+import json
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///task.db')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+SECRET_FILE = os.path.join(BASE_DIR, 'secrets.json')
+screts = json.loads(open(SECRET_FILE).read())
+DB = screts["DB"]
+
+DATABASE_URL =  f"mysql+pymysql://{DB['user']}:{DB['password']}@{DB['host']}:{DB['port']}/{DB['database']}?charset=utf8"
+# DATABASE_URL = "mysql+pymysql://root:yejoon135!@localhost:3306/db?charset=utf8"
+
+
+engine = create_engine(DATABASE_URL, encoding='utf-8')
 Session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 
--- models.py
+++ models.py
@@ -6,6 +6,6 @@ Base = declarative_base()
 class Task(Base):
     __tablename__ = 'tasks'
     id = Column(Integer, primary_key=True, index=True)
-    content = Column(String)
+    content = Column(String(32))
     created_at = Column(TIMESTAMP)
 
